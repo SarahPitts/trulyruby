8adfffc03abe6a954f9ac296d49650f63913c8a5
Sarah Pitts <mccallsmedia@gmail.com>
2013-08-07 14:56:40 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trulyruby::Application.config.secret_token = '821844fedced78ca5c17802b512f2a795213ccacf97ef8a812be1e3d8422eade78c77c1c8a2cababb43623fd6a0e9be25510e5ac21ca0c078bd5711bbc92cd27'
